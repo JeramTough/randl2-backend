16cd918a7c26de389ace547df03cceeda0fe1636
JeramTough <1171867004@qq.com>
2020-03-25 00:02:04 +0800

    Signed-off-by: JeramTough <1171867004@qq.com>

---
+++ webapp/src/main/java/com/jeramtough/randl2/component/registereduser/builder/RegisteredUserBuilder.java
@@ -0,0 +1,31 @@
+package com.jeramtough.randl2.component.registereduser.builder;
+
+import com.jeramtough.jtweb.component.apiresponse.exception.ApiResponseException;
+import com.jeramtough.randl2.dao.entity.RegisteredUser;
+
+/**
+ * <pre>
+ * Created on 2020/2/16 18:03
+ * by @author JeramTough
+ * </pre>
+ */
+public interface RegisteredUserBuilder extends UserBuilder {
+
+    /**
+     * 开始注册新用户事务
+     *
+     * @return 事务ID
+     */
+    String setAccount(String phoneOrEmailOrOther,
+                      int... errorCodes) throws ApiResponseException;
+
+
+    /**
+     * 确定构建新用户
+     */
+    RegisteredUser build(String transactionId, int... errorCodes) throws
+            ApiResponseException;
+
+
+}
+
